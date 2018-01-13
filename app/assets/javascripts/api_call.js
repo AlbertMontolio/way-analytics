d85aac3c98153497e83c31f5ab6efba8aa09f0ba
Albert Montolio <albertmontolio@Alberts-MacBook-Pro.local>
2018-01-13 12:16:56 +0100

    done

--- app/assets/javascripts/api_call.js
+++ app/assets/javascripts/api_call.js
@@ -3,7 +3,7 @@ const url = "http://localhost:3000/api/v1/profiles";
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("X-User-Email", "albert.montolio@waygroup.de");
-myHeaders.append("X-User-Token", "yJ1i5DsvwxEFHRheXNQ1");
+myHeaders.append("X-User-Token", "<%= sessionblablabla %>");
 
 const myInit = {
 	method: "GET",
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   protected
   def after_sign_in_path_for(resource)
   	# here i make the request to the waybook api
-  	
-  	
+
     request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
 end
